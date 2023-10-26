51c73d6c9730fe5e400918843d6a7ed19b2e550b
samade747 <samad.e747@gmail.com>
2023-10-27 00:05:28 +0500

    877557

--- app.js
+++ app.js
@@ -55,3 +55,53 @@ buttons.forEach(function(Button){
     buttonContainer.innerHTML += `<button class="btn btn-outline-primary" onclick="showItems('${button.toLowerCase()}')">${button}</button>`;
 });
 
+function showItems(category){
+    //function call on catoegory button is clicked or show all items 
+    const MenuContainer = document.getElementById('menu-items-display');
+    //getting the Menu items display container
+    MenuContainer.innerHTML = '';
+    // clearing data in MenuContainer
+
+    var filteredItems = foodMenu.filter(function(item){
+        if(item.itemCategory == category || category == 'All' ){
+            return true;
+        }
+    })
+
+    if(filteredItems.length === 0){
+        filteredItems = foodMenu;
+    }
+
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    filteredItems.forEach(function(item){
+        const col = document.createElement('div');
+        col.classList.add('col-md-4')
+        col.classList.add('col-sm-6')
+        col.classList.add('mb-3')
+
+        const div = document.createElement('div');
+        div.style.display = 'flex';
+        div.style.justifyContent = 'center';
+        div.style.alignItems = 'center';
+
+        div.innerHTML = `<div class="card" style="width: 40rem; border:5px solid skyblue; border-radius:30px; box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset; -webkit-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset; -moz-box-shadow: 100px 0px 58px -24px rgba(0,0,0,0.25) inset;">
+        <img class="card1img" style="border-radius:15px;" src="${item.itemimage}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-titleard1Name" style="color:rgb(202, 163, 90); text-transform:capitalize;">${item.itemName}</h5>
+            <h6 class="card-title card1Price" style="font-size:large;">RS : <span style="color:rgb(252, 103, );">${item.itemPrice}</span></h6>
+            <p class="card-text" style="color:red; font-size:medium;">${item.itemDescripition}</p>
+        </div>
+    </div>
+    `;
+
+    col.appendchild(div);
+    row.appendChild(col);
+
+
+    })
+
+    MenuContainer.appendChild(row);
+
+}
