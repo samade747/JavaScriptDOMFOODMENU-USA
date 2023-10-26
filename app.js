a27a3e3a77aeed6e822f13559cb5d614f0bc0351
samade747 <samad.e747@gmail.com>
2023-10-26 21:05:57 +0500

    225113

---
+++ app.js
@@ -0,0 +1,49 @@
+// creating a array of object of food items
+
+
+const foodMenu = [
+    // each object represent a food item
+    {
+     itemimage: './images/karai.jpg',
+     itemName : 'Karai',
+     itemPrice: '1200 PKR',
+     itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+     itemCategory: 'Dinner',
+     
+    },
+
+
+    {
+        itemimage: './images/omlet.jpg',
+        itemName : 'omlait Paratah',
+        itemPrice: '180 PKR',
+        itemDescripition: 'lorem lreeandadjjdsajkjsaddasasddsadsalknsasdasdandsa',
+        itemCategory: 'Breakfast',
+        
+       },
+
+
+       {
+        itemimage: './images/salad.jpg',
+        itemName : 'salad',
+        itemPrice: '150 PKR',
+        itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+        itemCategory: 'lunch',
+        
+       },
+
+       {
+        itemimage: './images/shake1.jpg',
+        itemName : 'Chocolate Shake',
+        itemPrice: '180 PKR',
+        itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+        itemCategory: 'shakes',
+        
+       },
+
+
+]
+
+//  Creating Buttons for food items display 
+const buttons = ['All', 'Breakfast', 'Lunch', 'shakes'];
+
